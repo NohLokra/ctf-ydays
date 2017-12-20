a4c5f15a86022cdd595eeeeb5f29ca16eae78eff
Zozoleclown <Zozoleclown1@gmail.com>
2017-12-20 17:48:54 +0100

    MaJ script SQL

--- ctf-ydays.sql
+++ ctf-ydays.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 22 Novembre 2017 à 16:01
+-- Généré le :  Mer 20 Décembre 2017 à 17:47
 -- Version du serveur :  5.6.17-log
 -- Version de PHP :  5.5.12
 
@@ -23,6 +23,67 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `acl`
+--
+
+CREATE TABLE IF NOT EXISTS `acl` (
+  `ai` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `action_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`ai`),
+  KEY `action_id` (`action_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `acl_actions`
+--
+
+CREATE TABLE IF NOT EXISTS `acl_actions` (
+  `action_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `action_code` varchar(100) NOT NULL COMMENT 'No periods allowed!',
+  `action_desc` varchar(100) NOT NULL COMMENT 'Human readable description',
+  `category_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`action_id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `acl_categories`
+--
+
+CREATE TABLE IF NOT EXISTS `acl_categories` (
+  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `category_code` varchar(100) NOT NULL COMMENT 'No periods allowed!',
+  `category_desc` varchar(100) NOT NULL COMMENT 'Human readable description',
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `category_code` (`category_code`),
+  UNIQUE KEY `category_desc` (`category_desc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `auth_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `auth_sessions` (
+  `id` varchar(128) NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `login_time` datetime DEFAULT NULL,
+  `modified_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ip_address` varchar(45) NOT NULL,
+  `user_agent` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `categories`
 --
 
@@ -33,18 +94,6 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
-
---
--- Contenu de la table `categories`
---
-
-INSERT INTO `categories` (`id`, `label`, `slug`, `description`) VALUES
-(1, 'Crack', 'crack', 'Les épreuves de cracking'),
-(2, 'Infra / Réseau', 'infra-reseau', 'Les épreuves d''infra et réseau'),
-(3, 'Programmation', 'programmation', 'Les épreuves de programmation'),
-(4, 'Shell', 'shell', 'Les épreuves en shell'),
-(5, 'Web Client', 'web-client', 'Les épreuves client web'),
-(6, 'Web Serveur', 'web-serveur', 'Les épreuves web orienté serveur');
 
 -- --------------------------------------------------------
 
@@ -58,8 +107,134 @@ CREATE TABLE IF NOT EXISTS `challenges` (
   `description` text NOT NULL,
   `password_hash` text NOT NULL,
   `category_id` int(11) NOT NULL,
+  `file` text,
+  `slug` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ci_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` blob NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ci_sessions_timestamp` (`timestamp`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `denied_access`
+--
+
+CREATE TABLE IF NOT EXISTS `denied_access` (
+  `ai` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(45) NOT NULL,
+  `time` datetime NOT NULL,
+  `reason_code` tinyint(1) unsigned DEFAULT '0',
+  PRIMARY KEY (`ai`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ips_on_hold`
+--
+
+CREATE TABLE IF NOT EXISTS `ips_on_hold` (
+  `ai` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(45) NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`ai`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `login_errors`
+--
+
+CREATE TABLE IF NOT EXISTS `login_errors` (
+  `ai` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username_or_email` varchar(255) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`ai`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `username_or_email_on_hold`
+--
+
+CREATE TABLE IF NOT EXISTS `username_or_email_on_hold` (
+  `ai` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username_or_email` varchar(255) NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`ai`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(10) unsigned NOT NULL,
+  `username` varchar(12) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `auth_level` tinyint(3) unsigned NOT NULL,
+  `banned` enum('0','1') NOT NULL DEFAULT '0',
+  `passwd` varchar(60) NOT NULL,
+  `passwd_recovery_code` varchar(60) DEFAULT NULL,
+  `passwd_recovery_date` datetime DEFAULT NULL,
+  `passwd_modified_at` datetime DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `modified_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déclencheurs `users`
+--
+DROP TRIGGER IF EXISTS `ca_passwd_trigger`;
+DELIMITER //
+CREATE TRIGGER `ca_passwd_trigger` BEFORE UPDATE ON `users`
+ FOR EACH ROW BEGIN
+    IF ((NEW.passwd <=> OLD.passwd) = 0) THEN
+        SET NEW.passwd_modified_at = NOW();
+    END IF;
+END
+//
+DELIMITER ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `acl`
+--
+ALTER TABLE `acl`
+  ADD CONSTRAINT `acl_ibfk_1` FOREIGN KEY (`action_id`) REFERENCES `acl_actions` (`action_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `acl_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `acl_actions`
+--
+ALTER TABLE `acl_actions`
+  ADD CONSTRAINT `acl_actions_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `acl_categories` (`category_id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

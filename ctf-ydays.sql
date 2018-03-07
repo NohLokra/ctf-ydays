61047d7e25dc5a98086cee64c884f55c25f7ef03
Zozoleclown <Zozoleclown1@gmail.com>
2018-03-07 12:20:35 +0100

    MaJ script SQL pour prod

--- ctf-ydays.sql
+++ ctf-ydays.sql
@@ -1,77 +1,109 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
+-- MySQL dump 10.16  Distrib 10.1.29-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Client :  127.0.0.1
--- Généré le :  Mer 07 Mars 2018 à 09:13
--- Version du serveur :  5.6.17-log
--- Version de PHP :  5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: ctf-ydays
+-- ------------------------------------------------------
+-- Server version	10.1.29-MariaDB-6
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données :  `ctf-ydays`
+-- Table structure for table `acl`
 --
 
--- --------------------------------------------------------
-
---
--- Structure de la table `acl`
---
-
-CREATE TABLE IF NOT EXISTS `acl` (
+DROP TABLE IF EXISTS `acl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `acl` (
   `ai` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `action_id` int(10) unsigned NOT NULL,
   `user_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`ai`),
   KEY `action_id` (`action_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `acl_ibfk_1` FOREIGN KEY (`action_id`) REFERENCES `acl_actions` (`action_id`) ON DELETE CASCADE,
+  CONSTRAINT `acl_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `acl_actions`
+-- Dumping data for table `acl`
 --
 
-CREATE TABLE IF NOT EXISTS `acl_actions` (
+LOCK TABLES `acl` WRITE;
+/*!40000 ALTER TABLE `acl` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acl` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `acl_actions`
+--
+
+DROP TABLE IF EXISTS `acl_actions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `acl_actions` (
   `action_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `action_code` varchar(100) NOT NULL COMMENT 'No periods allowed!',
   `action_desc` varchar(100) NOT NULL COMMENT 'Human readable description',
   `category_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`action_id`),
-  KEY `category_id` (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `acl_actions_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `acl_categories` (`category_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `acl_categories`
+-- Dumping data for table `acl_actions`
 --
 
-CREATE TABLE IF NOT EXISTS `acl_categories` (
+LOCK TABLES `acl_actions` WRITE;
+/*!40000 ALTER TABLE `acl_actions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acl_actions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `acl_categories`
+--
+
+DROP TABLE IF EXISTS `acl_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `acl_categories` (
   `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `category_code` varchar(100) NOT NULL COMMENT 'No periods allowed!',
   `category_desc` varchar(100) NOT NULL COMMENT 'Human readable description',
   PRIMARY KEY (`category_id`),
   UNIQUE KEY `category_code` (`category_code`),
   UNIQUE KEY `category_desc` (`category_desc`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `auth_sessions`
+-- Dumping data for table `acl_categories`
 --
 
-CREATE TABLE IF NOT EXISTS `auth_sessions` (
+LOCK TABLES `acl_categories` WRITE;
+/*!40000 ALTER TABLE `acl_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `acl_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auth_sessions`
+--
+
+DROP TABLE IF EXISTS `auth_sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `auth_sessions` (
   `id` varchar(128) NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
   `login_time` datetime DEFAULT NULL,
@@ -80,40 +112,52 @@ CREATE TABLE IF NOT EXISTS `auth_sessions` (
   `user_agent` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `categories`
+-- Dumping data for table `auth_sessions`
 --
 
-CREATE TABLE IF NOT EXISTS `categories` (
+LOCK TABLES `auth_sessions` WRITE;
+/*!40000 ALTER TABLE `auth_sessions` DISABLE KEYS */;
+INSERT INTO `auth_sessions` VALUES ('bed01d57e5c07257d11932f8139cb9cc9e7c7a26',2928008995,'2018-03-07 11:01:58','2018-03-07 10:47:39','127.0.0.1','Firefox 52.0 on Linux'),('5aa298a1596ab0df4bc6a0797c84839e02a06418',2928008995,'2018-03-07 12:05:28','2018-03-07 11:11:35','127.0.0.1','Firefox 52.0 on Linux'),('2884af445abc2ccf6b56cc813ec87fb6af89f6c9',1581586112,'2018-03-07 12:13:57','2018-03-07 11:13:57','127.0.0.1','Firefox 52.0 on Linux');
+/*!40000 ALTER TABLE `auth_sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(50) NOT NULL,
   `slug` varchar(50) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contenu de la table `categories`
+-- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id`, `label`, `slug`, `description`) VALUES
-(1, 'Crack', 'crack', 'Les épreuves de cracking'),
-(2, 'Infra / Réseau', 'infra-reseau', 'Les épreuves d''infra et réseau'),
-(3, 'Programmation', 'programmation', 'Les épreuves de programmation'),
-(4, 'Shell', 'shell', 'Les épreuves en shell'),
-(5, 'Web Client', 'web-client', 'Les épreuves client web'),
-(6, 'Web Serveur', 'web-serveur', 'Les épreuves web orienté serveur');
-
--- --------------------------------------------------------
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Crack','crack','Les épreuves de cracking'),(2,'Infra / Réseau','infra-reseau','Les épreuves d\'infra et réseau'),(3,'Programmation','programmation','Les épreuves de programmation'),(4,'Shell','shell','Les épreuves en shell'),(5,'Web Client','web-client','Les épreuves client web'),(6,'Web Serveur','web-serveur','Les épreuves web orienté serveur');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `challenges`
+-- Table structure for table `challenges`
 --
 
-CREATE TABLE IF NOT EXISTS `challenges` (
+DROP TABLE IF EXISTS `challenges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `challenges` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(50) NOT NULL,
   `description` text NOT NULL,
@@ -124,26 +168,27 @@ CREATE TABLE IF NOT EXISTS `challenges` (
   `file` text,
   `slug` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contenu de la table `challenges`
+-- Dumping data for table `challenges`
 --
 
-INSERT INTO `challenges` (`id`, `label`, `description`, `active`, `removed`, `password_hash`, `category_id`, `file`, `slug`) VALUES
-(1, 'test', 'Une superbe description pour ce fantastique challenge de test', 1, 0, '$2y$11$llfsICJ0d6dSoKqfV55s/eZVftYtPhHmOz1UkqEEMIE2zLCNGSh/6', 4, NULL, 'test'),
-(2, 'test', 'test', 1, 0, '$2y$11$qHadAWMOSU24UB8Z0niYzOKSWoE/JKNmQmbPWCFCr6VGjWERYEbRe', 4, '4_2_22.zip', 'test-1'),
-(3, 'test', 'test', 1, 0, '$2y$11$DTzZio4L4sOGPCN1cakcBeBBWKIqNoAP9Rf1FDokG/7wmFa0i3XnW', 4, '4_2_23.zip', 'test-2'),
-(4, 'Premier test', 'Le mot de passe est stocké dans un fichier lisible par un humain... Démerdez-vous avec ça les poulets !', 1, 0, '$2y$11$p7NRXjpRPwi0HeWIWqKRQOu2gOprB8pzPsC8IYRIzRHFw.HQl36oe', 4, NULL, 'premier-test'),
-(5, 'Un simple mot de passe', 'Dans l''archive donnée ici, il vous faudra contourner l''authentification de ce simple logiciel console.', 1, 0, '$2y$11$zueKK1KKidxU750UZmhk9uqwX51VDcXNgqMOwc2ju7fJ0M8J.8qmO', 1, 'crackme1.zip', 'un-simple-mot-de-passe');
-
--- --------------------------------------------------------
+LOCK TABLES `challenges` WRITE;
+/*!40000 ALTER TABLE `challenges` DISABLE KEYS */;
+INSERT INTO `challenges` VALUES (1,'test','Une superbe description pour ce fantastique challenge de test',1,0,'$2y$11$llfsICJ0d6dSoKqfV55s/eZVftYtPhHmOz1UkqEEMIE2zLCNGSh/6',4,NULL,'test'),(2,'test','test',1,0,'$2y$11$qHadAWMOSU24UB8Z0niYzOKSWoE/JKNmQmbPWCFCr6VGjWERYEbRe',4,'4_2_22.zip','test-1'),(3,'test','test',1,0,'$2y$11$DTzZio4L4sOGPCN1cakcBeBBWKIqNoAP9Rf1FDokG/7wmFa0i3XnW',4,'4_2_23.zip','test-2'),(4,'Premier test','Le mot de passe est stocké dans un fichier lisible par un humain... Démerdez-vous avec ça les poulets !',1,0,'$2y$11$p7NRXjpRPwi0HeWIWqKRQOu2gOprB8pzPsC8IYRIzRHFw.HQl36oe',4,NULL,'premier-test'),(5,'Un simple mot de passe','Dans l\'archive donnée ici, il vous faudra contourner l\'authentification de ce simple logiciel console.',1,0,'$2y$11$zueKK1KKidxU750UZmhk9uqwX51VDcXNgqMOwc2ju7fJ0M8J.8qmO',1,'crackme1.zip','un-simple-mot-de-passe');
+/*!40000 ALTER TABLE `challenges` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `ci_sessions`
+-- Table structure for table `ci_sessions`
 --
 
-CREATE TABLE IF NOT EXISTS `ci_sessions` (
+DROP TABLE IF EXISTS `ci_sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ci_sessions` (
   `id` varchar(128) NOT NULL,
   `ip_address` varchar(45) NOT NULL,
   `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
@@ -151,68 +196,124 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   PRIMARY KEY (`id`),
   KEY `ci_sessions_timestamp` (`timestamp`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `denied_access`
+-- Dumping data for table `ci_sessions`
 --
 
-CREATE TABLE IF NOT EXISTS `denied_access` (
+LOCK TABLES `ci_sessions` WRITE;
+/*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `denied_access`
+--
+
+DROP TABLE IF EXISTS `denied_access`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `denied_access` (
   `ai` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(45) NOT NULL,
   `time` datetime NOT NULL,
   `reason_code` tinyint(1) unsigned DEFAULT '0',
   PRIMARY KEY (`ai`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `ips_on_hold`
+-- Dumping data for table `denied_access`
 --
 
-CREATE TABLE IF NOT EXISTS `ips_on_hold` (
+LOCK TABLES `denied_access` WRITE;
+/*!40000 ALTER TABLE `denied_access` DISABLE KEYS */;
+/*!40000 ALTER TABLE `denied_access` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ips_on_hold`
+--
+
+DROP TABLE IF EXISTS `ips_on_hold`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ips_on_hold` (
   `ai` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `ip_address` varchar(45) NOT NULL,
   `time` datetime NOT NULL,
   PRIMARY KEY (`ai`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `login_errors`
+-- Dumping data for table `ips_on_hold`
 --
 
-CREATE TABLE IF NOT EXISTS `login_errors` (
+LOCK TABLES `ips_on_hold` WRITE;
+/*!40000 ALTER TABLE `ips_on_hold` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ips_on_hold` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login_errors`
+--
+
+DROP TABLE IF EXISTS `login_errors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login_errors` (
   `ai` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username_or_email` varchar(255) NOT NULL,
   `ip_address` varchar(45) NOT NULL,
   `time` datetime NOT NULL,
   PRIMARY KEY (`ai`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `username_or_email_on_hold`
+-- Dumping data for table `login_errors`
 --
 
-CREATE TABLE IF NOT EXISTS `username_or_email_on_hold` (
+LOCK TABLES `login_errors` WRITE;
+/*!40000 ALTER TABLE `login_errors` DISABLE KEYS */;
+INSERT INTO `login_errors` VALUES (1,'Hollow','127.0.0.1','2018-03-07 12:13:37');
+/*!40000 ALTER TABLE `login_errors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `username_or_email_on_hold`
+--
+
+DROP TABLE IF EXISTS `username_or_email_on_hold`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `username_or_email_on_hold` (
   `ai` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username_or_email` varchar(255) NOT NULL,
   `time` datetime NOT NULL,
   PRIMARY KEY (`ai`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `users`
+-- Dumping data for table `username_or_email_on_hold`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+LOCK TABLES `username_or_email_on_hold` WRITE;
+/*!40000 ALTER TABLE `username_or_email_on_hold` DISABLE KEYS */;
+/*!40000 ALTER TABLE `username_or_email_on_hold` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `user_id` int(10) unsigned NOT NULL,
   `username` varchar(12) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
@@ -229,45 +330,45 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Contenu de la table `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `email`, `auth_level`, `banned`, `passwd`, `passwd_recovery_code`, `passwd_recovery_date`, `passwd_modified_at`, `last_login`, `created_at`, `modified_at`) VALUES
-(2147484848, 'Zozo', 'pierrick.couget@ynov.com', 9, '0', '$2y$11$RlSJO4pLUw1u5hFQtOtcqORkK1e.okuLy5uEmJLN0mfyllU9Hl7iC', NULL, NULL, NULL, '2018-01-24 15:45:17', '2017-12-06 15:05:59', '2018-01-24 14:45:17');
-
---
--- Déclencheurs `users`
---
-DROP TRIGGER IF EXISTS `ca_passwd_trigger`;
-DELIMITER //
-CREATE TRIGGER `ca_passwd_trigger` BEFORE UPDATE ON `users`
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1581586112,'Hollow','loic.huon@ynov.com',9,'0','$2y$11$57BjmKJbCoqiubDqgBXCWec5O5LOlxCmE5cnVBT4kZPKk1/HZvMAW',NULL,NULL,NULL,'2018-03-07 12:13:57','2018-03-07 12:13:11','2018-03-07 11:15:21'),(2147484848,'Zozo','pierrick.couget@ynov.com',9,'0','$2y$11$RlSJO4pLUw1u5hFQtOtcqORkK1e.okuLy5uEmJLN0mfyllU9Hl7iC',NULL,NULL,NULL,'2018-01-24 15:45:17','2017-12-06 15:05:59','2018-01-24 14:45:17');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`%`*/ /*!50003 TRIGGER `ca_passwd_trigger` BEFORE UPDATE ON `users`
  FOR EACH ROW BEGIN
     IF ((NEW.passwd <=> OLD.passwd) = 0) THEN
         SET NEW.passwd_modified_at = NOW();
     END IF;
-END
-//
+END */;;
 DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Contraintes pour les tables exportées
---
-
---
--- Contraintes pour la table `acl`
---
-ALTER TABLE `acl`
-  ADD CONSTRAINT `acl_ibfk_1` FOREIGN KEY (`action_id`) REFERENCES `acl_actions` (`action_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `acl_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
-
---
--- Contraintes pour la table `acl_actions`
---
-ALTER TABLE `acl_actions`
-  ADD CONSTRAINT `acl_actions_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `acl_categories` (`category_id`) ON DELETE CASCADE;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-07 12:18:39

bddd2c3afec69d1f41d5fb0275cf1545ad6ab532
Zozoleclown <Zozoleclown1@gmail.com>
2018-04-04 15:45:53 +0200

    Génération du slug à la création d'un challenge

--- application/core/DB_Model.php
+++ application/core/DB_Model.php
@@ -1,121 +1,145 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class DB_Model extends CI_Model {
-  protected $table;
-  protected $database;
+class DB_Model extends CI_Model
+{
+    protected $table;
+    protected $database;
 
-  public function __construct($database, $table, $id_col = "id", $file_columns = []) {
-    parent::__construct();
+    public function __construct($database, $table, $id_col = "id", $file_columns = [])
+    {
+        parent::__construct();
 
-    $this->database = $database;
-    $this->table = $table;
-    $this->id_column = $id_col;
-    $this->file_columns = $file_columns;
+        $this->database = $database;
+        $this->table = $table;
+        $this->id_column = $id_col;
+        $this->file_columns = $file_columns;
 
-    $this->load->database($database);
+        $this->load->database($database);
 
-    $this->load->helper("url");
-    $this->load->config('uploads', true);
-  }
-
-  public function get($id) {
-    $record = $this->db->where($this->id_column, $id)->get($this->table)->row();
-    log_message("debug", "[DB_Model] Récupération de $id dans la table $this->table: " . json_encode($record)); // Ici on a bien une valeur et c'est bien cette fonction qui est appelée car on le log
-
-    return $record;
-  }
-
-  public function getAll() {
-    return $this->db->get($this->table)->result();
-  }
-
-  public function create($data) { // $data peut être une table associative ou un objet
-    $data = (array)$data;
-    $data = $this->store_files($data);
-
-    if ( $this->db->insert($this->table, $data) ) {
-      return $this->get($this->db->insert_id());
-    } else {
-      log_message('error', 'Error when inserting record in table ' . strtolower($this->table) . '\nRecord infos:\n\t' . implode("\n\t", $data));
-      return false;
-    }
-  }
-
-  public function update($id, $data) { // $data peut $être une table associative ou un objet
-    log_message('debug', '[DB_Model] Demande de mise à jour du produit ' . $id);
-
-    $data = (array)$data;
-    $data = $this->store_files($data);
-
-    if ( count($this->file_columns) > 0 ) {
-      $record = $this->get($id);
+        $this->load->helper("url");
+        $this->load->config('uploads', true);
     }
 
-    if ( $this->db->where($this->id_column, $id)->update($this->table, $data) ) {
-      return $id;
-    } else {
-      log_message('error', 'Error when updating record in table ' . strtolower($this->table) . '\nRecord infos:\n\t' . implode("\n\t", $data));
-      return false;
-    }
-  }
+    public function get($id)
+    {
+        $record = $this->db->where($this->id_column, $id)->get($this->table)->row();
+        log_message("debug", "[DB_Model] Récupération de $id dans la table $this->table: " . json_encode($record)); // Ici on a bien une valeur et c'est bien cette fonction qui est appelée car on le log
 
-  public function delete($id) {
-    if ( count($this->file_columns) > 0 ) {
-      $record = $this->get($id);
-
-      foreach ( $this->file_columns as $file_column ) {
-        $filename = $record->$file_column;
-        $filepath = $this->config->config["upload_path"] . $filename;
-
-          if ( file_exists($filepath) )
-            unlink($filepath);
-      }
+        return $record;
     }
 
-    if ($this->db->where($this->id_column, $id)->delete($this->table)) {
-      return true;
-    } else {
-      log_message('error', 'Error when deleting record ' . $id . ' in table ' . strtolower($this->table));
-      return false;
+    public function getAll()
+    {
+        return $this->db->get($this->table)->result();
     }
-  }
 
-  public function select_distinct($column_name) {
-    return $this->db->distinct()->select($column_name)->get($this->table)->result();
-  }
+    public function create($data)
+    { // $data peut être une table associative ou un objet
+        $data = (array) $data;
+        $data = $this->store_files($data);
 
-  public function exists($id) {
-    $rec = $this->db->where($this->id_column, $id)->get($this->table)->result();
-    if ( $rec )
-      return true;
-    else
-      return false;
-  }
-
-  public function search($searchArray) {
-    return $this->db->where($searchArray)->get($this->table)->result();
-  }
-
-  protected function store_files($data) {
-    $this->load->library('upload', $this->config->config['uploads']);
-
-    foreach ( $this->file_columns as $fc ) {
-      if (!empty($_FILES[$fc]['name'])) {
-        $challenge[$fc] = $_FILES[$fc];
-
-        if ( $this->upload->do_upload($fc) ) {
-          $upload_result = $this->upload->data();
-          $data[$fc] = $upload_result['file_name'];
+        if ($this->db->insert($this->table, $data)) {
+            return $this->get($this->db->insert_id());
         } else {
-          $error = array('error' => $this->upload->display_errors());
-
-          throw new Exception(print_r($error, true), 1);
+            log_message('error', 'Error when inserting record in table ' . strtolower($this->table) . '\nRecord infos:\n\t' . implode("\n\t", $data));
+            return false;
         }
-      }
     }
 
-    return $data;
-  }
+    public function update($id, $data)
+    { // $data peut $être une table associative ou un objet
+        log_message('debug', '[DB_Model] Demande de mise à jour du produit ' . $id);
+
+        $data = (array) $data;
+        $data = $this->store_files($data);
+
+        if (count($this->file_columns) > 0) {
+            $record = $this->get($id);
+        }
+
+        if ($this->db->where($this->id_column, $id)->update($this->table, $data)) {
+            return $id;
+        } else {
+            log_message('error', 'Error when updating record in table ' . strtolower($this->table) . '\nRecord infos:\n\t' . implode("\n\t", $data));
+            return false;
+        }
+    }
+
+    public function delete($id)
+    {
+        if (count($this->file_columns) > 0) {
+            $record = $this->get($id);
+
+            foreach ($this->file_columns as $file_column) {
+                $filename = $record->$file_column;
+                $filepath = $this->config->config["upload_path"] . $filename;
+
+                if (file_exists($filepath)) {
+                    unlink($filepath);
+                }
+
+            }
+        }
+
+        if ($this->db->where($this->id_column, $id)->delete($this->table)) {
+            return true;
+        } else {
+            log_message('error', 'Error when deleting record ' . $id . ' in table ' . strtolower($this->table));
+            return false;
+        }
+    }
+
+    public function select_distinct($column_name)
+    {
+        return $this->db->distinct()->select($column_name)->get($this->table)->result();
+    }
+
+    public function exists($id)
+    {
+        $rec = $this->db->where($this->id_column, $id)->get($this->table)->result();
+        if ($rec) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public function search($searchArray)
+    {
+        return $this->db->where($searchArray)->get($this->table)->result();
+    }
+
+    protected function store_files($data)
+    {
+        $this->load->library('upload', $this->config->config['uploads']);
+
+        foreach ($this->file_columns as $fc) {
+            if (!empty($_FILES[$fc]['name'])) {
+                $challenge[$fc] = $_FILES[$fc];
+
+                if ($this->upload->do_upload($fc)) {
+                    $upload_result = $this->upload->data();
+                    $data[$fc] = $upload_result['file_name'];
+                } else {
+                    $error = array('error' => $this->upload->display_errors());
+
+                    throw new Exception(print_r($error, true), 1);
+                }
+            }
+        }
+
+        return $data;
+    }
+
+    protected function to_slug($str)
+    {
+        $this->load->helper('url');
+
+        $slug = url_title($str);
+
+        return $slug;
+    }
 
 }
--- application/models/Challenges_model.php
+++ application/models/Challenges_model.php
@@ -1,46 +1,65 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Challenges_model extends MY_Model{
+class Challenges_model extends MY_Model
+{
 
-  public function __construct() {
-    parent::__construct("challenges", "id", ["file"]);
-  }
+    public function __construct()
+    {
+        parent::__construct("challenges", "id", ["file"]);
+    }
 
-  public function getForCategory($cat_id) {
-    return $this->db->where('category_id', $cat_id)->get($this->table)->result();
-  }
+    public function getForCategory($cat_id)
+    {
+        return $this->db->where('category_id', $cat_id)->get($this->table)->result();
+    }
 
-  public function getBySlug($slug) {
-    return $this->search([
-      "slug" => $slug
-    ])[0];
-  }
+    public function getBySlug($slug)
+    {
+        return $this->search([
+            "slug" => $slug,
+        ])[0];
+    }
 
-  public function checkPasswordBySlug($slug, $flag) {
-    $challenge = $this->getBySlug($slug);
+    public function checkPasswordBySlug($slug, $flag)
+    {
+        $challenge = $this->getBySlug($slug);
 
-    return $this->authentication->check_passwd($challenge->hashed_password, $flag);
-  }
+        return $this->authentication->check_passwd($challenge->hashed_password, $flag);
+    }
 
-  public function getAllToValidate() {
-    return $this->search([
-      "active" => 0,
-      "removed" => 0
-    ]);
-  }
+    public function getAllToValidate()
+    {
+        return $this->search([
+            "active" => 0,
+            "removed" => 0,
+        ]);
+    }
 
-  public function removeInactive($id) {
-    return $this->update($id, [
-      "removed" => 1
-    ]);
-  }
+    public function removeInactive($id)
+    {
+        return $this->update($id, [
+            "removed" => 1,
+        ]);
+    }
 
-  public function validate($id) {
-    return $this->update($id, [
-      "removed" => 0,
-      "active" => 1
-    ]);
-  }
+    public function validate($id)
+    {
+        return $this->update($id, [
+            "removed" => 0,
+            "active" => 1
+        ]);
+    }
+
+    public function create($data)
+    {
+        $data = (array) $data;
+
+        $slug = parent::to_slug($data['label']);
+
+        $data['slug'] = $slug;
+
+        parent::create($data);
+    }
 
 }
